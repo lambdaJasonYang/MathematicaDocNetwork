3e1e0cd5009a00c8ba9f2a91bd9f84f2b1d82e23
Your Name <you@example.com>
2022-10-17 14:19:50 -0400

    up

--- next.config.js
+++ next.config.js
@@ -3,16 +3,19 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 let isProd = process.env.myEnv === "prod";
+const path = require("path")
 
-console.log(process.env)
 module.exports = (phase, { defaultConfig }) => {
+  
   if (phase === PHASE_DEVELOPMENT_SERVER){ isProd = false };  
+  //console.log("HI",defaultConfig)
   
   return {
     /* config options for all phases except development here */
     reactStrictMode: true,
     assetPrefix: isProd ? "/MathematicaDocNetwork" : undefined, //static assets are in the /out folder
     basePath: isProd ? "/MathematicaDocNetwork" : undefined,
+    
     experimental: {
       images: {
         unoptimized: true,
@@ -20,3 +23,4 @@ module.exports = (phase, { defaultConfig }) => {
     }
   }
 }
+
